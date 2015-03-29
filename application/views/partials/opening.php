8d9a15ccebb0be8e0d6062d2da947d843cb85e3c
Keith Certeza Darunday <jkcdarunday@gmail.com>
2015-03-30 01:12:05 +0800

    Added partial views

---
+++ application/views/partials/opening.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>
+		<title></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="<?php echo base_url('resources/css/pure-min.css'); ?>">
+		<link rel="stylesheet" href="<?php echo base_url('resources/css/grids-responsive-min.css'); ?>">
+		<link rel="stylesheet" href="<?php echo base_url('resources/css/style.css'); ?>">
+		<script src="<?php echo base_url('resources/js/schedule-renderer.js'); ?>"></script>
+	</head>
+	<body>
+		<div class="pure-g">
+			<div class="menubar pure-u-1 pure-u-md-1-5">
+				<div class="menu">
+					<div class="logo"><img src="<?php echo base_url('resources/images/takda.png'); ?>"></div>
+					<div class="title">TaKDa</div>
+					<div class="subtitle">A Course and Teaching Load Management System</div>
+
+					<nav class="nav">
+						<a class="pure-button" href="<?php echo site_url('Home'); ?>">Home</a>
+				</div>
+			</div>
