c273351e5f3245aa9416d3046e9f98f1df031230
Keith Certeza Darunday <jkcdarunday@gmail.com>
2015-04-14 14:03:36 +0800

    Added a label to the parse button

--- application/views/schedule.php
+++ application/views/schedule.php
@@ -1,6 +1,6 @@
 <div id="input">
 	<input type="text" id="schedInput"></input>
-	<button onClick="parseString(document.getElementById('schedInput').value);"></button>
+	<button onClick="parseString(document.getElementById('schedInput').value);">Parse Schedule</button>
 </div>
 <div id="canvascontainer">
 	<canvas name="sched" id="sched" width="640px" height="480px"></canvas>
