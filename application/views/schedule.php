5c5da2b1fad7581779c3f2e2464bbbec9831cc63
Keith Certeza Darunday <jkcdarunday@gmail.com>
2015-04-14 14:00:02 +0800

    Added test view for the schedule viewer

--- application/views/schedule.php
+++ application/views/schedule.php
@@ -1,1 +1,7 @@
+<div id="input">
+	<input type="text" id="schedInput"></input>
+	<button onClick="parseString(document.getElementById('schedInput').value);"></button>
+</div>
+<div id="canvascontainer">
 	<canvas name="sched" id="sched" width="640px" height="480px"></canvas>
+</div>
