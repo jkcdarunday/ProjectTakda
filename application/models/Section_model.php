180af539cab80eca203af323740e650b157890c5
Keith Certeza Darunday <jkcdarunday@gmail.com>
2015-04-21 03:26:01 +0800

    Implemented search for Section model

--- application/models/Section_model.php
+++ application/models/Section_model.php
@@ -1,8 +1,15 @@
 <?php
 class Section_model extends CI_Model {
 
-	function search($keyword){
-
+	function search($keyword){ 
+		$this->db->select('schedule, courseCode, sectionCode, roomName, firstName, middleName, lastName, classification');
+		$this->db->from('section');
+		$this->db->like('courseCode',$keyword);
+		$this->db->or_like('courseTitle',$keyword);
+		$this->db->join('room', 'section.roomId = room.roomId');
+		$this->db->join('user', 'section.employeeId = user.employeeId');
+		$query = $this->db->get();
+		return $query->result();
 	}
 
 }
