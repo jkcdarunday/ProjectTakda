76f9802609a6e9005c387f5434f8317a8e0696d4
Keith Certeza Darunday <jkcdarunday@gmail.com>
2015-04-21 03:42:50 +0800

    Search through other displayed information too

--- application/models/Section_model.php
+++ application/models/Section_model.php
@@ -6,6 +6,11 @@ class Section_model extends CI_Model {
 		$this->db->from('section');
 		$this->db->like('section.courseCode',$keyword);
 		$this->db->or_like('courseTitle',$keyword);
+		$this->db->or_like('section.sectionCode',$keyword);
+		$this->db->or_like('user.firstName',$keyword);
+		$this->db->or_like('user.middleName',$keyword);
+		$this->db->or_like('user.lastName',$keyword);
+		$this->db->or_like('classification',$keyword);
 		$this->db->join('room', 'section.roomId = room.roomId');
 		$this->db->join('subject', 'section.courseCode = subject.courseCode');
 		$this->db->join('user', 'section.employeeId = user.employeeId');
