d68a1afdb9f76d7ce3958bc37615a73ce1e48b7e
Keith Certeza Darunday <jkcdarunday@gmail.com>
2015-04-21 03:34:57 +0800

    Passed query results to the view

--- application/views/schedule.php
+++ application/views/schedule.php
@@ -1,4 +1,4 @@
-<div id="input">
+<!--<div id="input">
 	<form method="get" action="<?php echo site_url('Schedule/employeeSchedule'); ?>">
 	<input type="text" name="eid" placeholder="Employee ID" value='<?php if(isset($_GET['eid'])) echo $_GET['eid']; ?>'></input>
 	<input type="submit"></input>
@@ -7,7 +7,31 @@
 	<input type="text" name="roomid" placeholder="room ID" value='<?php if(isset($_GET['roomid'])) echo $_GET['roomid']; ?>'></input>
 	<input type="submit"></input>
 	</form>
-</div>
+</div>-->
+<table class="pure-table pure-table-bordered">
+    <thead>
+        <tr>
+            <th>Course Code</th>
+            <th>Section Code</th>
+            <?php if(isset($type) && $type=="employee"){?>
+            <th>Room</th>
+            <?php } else { ?>
+            <th>Instructor</th>
+            <?php } ?>
+        </tr>
+    </thead>
+
+    <tbody>
+<?php foreach($schedules as $item): ?>
+        <tr>
+            <td><?php echo $item->courseCode; ?></td>
+            <td><?php echo $item->sectionCode; ?></td>
+            <td><?php if($type=="employee") echo $item->roomName; else echo $item->username; ?></td>
+        </tr>
+<?php endforeach; ?>
+	</tbody>
+</table>
+	
 <div id="canvascontainer">
 	<canvas name="sched" id="sched" width="640px" height="480px"></canvas>
 	<script>
