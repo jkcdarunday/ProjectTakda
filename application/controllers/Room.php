01afabd1731d83714905146cf7c94204c3badc50
Keith Certeza Darunday <jkcdarunday@gmail.com>
2015-04-21 03:26:58 +0800

    Added template controllers for Course, Section, and Room

---
+++ application/controllers/Room.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Room extends CI_Controller
+{
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * This controller is the default controller set in config/routes.php.
+	 * It will be shown whenever the root directory of the website is accessed.
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->helper('url');
+		
+		$this->load->view('partials/opening');
+		$this->load->view('room');
+		$this->load->view('partials/closing');
+	}
+}
