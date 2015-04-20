1d49e9408b92f4a2d06e1f180cd177af12ba50ac
Keith Certeza Darunday <jkcdarunday@gmail.com>
2015-04-21 03:43:01 +0800

    Implemented search for the Room model

---
+++ application/models/Room_model.php
@@ -0,0 +1,15 @@
+<?php
+class Room_model extends CI_Model {
+
+	function search($keyword){ 
+		$this->db->select('roomID, roomName, seatingCapacity, building, floor');
+		$this->db->from('room');
+		$this->db->like('roomName',$keyword);
+		$this->db->or_like('building',$keyword);
+		$this->db->or_like('Floor',$keyword);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+}
+?>
