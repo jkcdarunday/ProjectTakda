7e00e5b935b09cb60b6682a7b0bfb08513f7cd5b
Keith Certeza Darunday <jkcdarunday@gmail.com>
2015-04-21 05:38:52 +0800

    Added user search veiw

---
+++ application/views/user.php
@@ -0,0 +1,37 @@
+<form class="pure-form" method="get" action="?">
+    <fieldset>
+        <legend>Search Users</legend>
+        <input name="query" id="query" type="text" placeholder="Section Name">
+        <input name="type" id="type" type="hidden" value="search">
+        <button type="submit" class="pure-button pure-button-primary">Search</button>
+	</fieldset>
+</form>
+<table class="pure-table pure-table-bordered full-width">
+    <thead>
+        <tr>
+            <th>Employee ID</th>
+            <th>Username</th>
+            <th>Email Address</th>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+
+    <tbody>
+<?php if(isset($users)) foreach($users as $item): ?>
+        <tr>
+            <td><?php echo $item->employeeID; ?></td>
+            <td><?php echo $item->username; ?></td>
+            <td><?php echo $item->emailAddress; ?></td>
+            <td><?php echo $item->firstName . " " . $item->middleName . " " . $item->lastName; ?></td>
+            <td><?php echo $item->position; ?></td>
+            <td>
+				<a class="pure-button pure-button-primary small-font" href="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
+				<a class="pure-button pure-button-primary small-font" href="swap"><i class="fa fa-arrows-h"></i> Swap</a>
+				<a class="pure-button pure-button-primary button-error small-font" href="drop"><i class="fa fa-times"></i> Delete</a>
+				</td>
+        </tr>
+<?php endforeach; ?>
+	</tbody>
+</table>
